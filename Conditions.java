a59ebf0cdb91bcdc7c919167f1002287f70834f0
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-26 14:46:27 -0500

    On branch main 	modified:   Conditions.java 	modified:   Function.java

--- Conditions.java
+++ Conditions.java
@@ -1,17 +1,20 @@
+/*
+ * Learing conditions in java. 
+ */
 import java.util.*;
 
 public class Conditions {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number!");
+            int num = sc.nextInt();
 
-        System.out.println("Enter a number!");
-        int num = sc.nextInt();
-
-        if (num%2==0){
-            System.out.println("Even number");
-        } else{
-            System.out.println("Odd number");
+            if (num%2==0){
+                System.out.println("Even number");
+            } else{
+                System.out.println("Odd number");
+            }
+            sc.close();
         }
-
     }
 }
--- Function.java
+++ Function.java
@@ -1,14 +1,16 @@
-//Calculating factorial of n number.
+/*
+ * Learing function in java.
+ */
 import java.util.*;
 public class Function {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a num..!");
-        int n = sc.nextInt();
-        int result = fac(n);
-        System.out.println(result);
-
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a num..!");
+            int n = sc.nextInt();
+            int result = fac(n);
+            System.out.println(result);
+            sc.close();
+        }
     }
     public static int fac(int n){
         if(n<1){
