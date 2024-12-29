b4e57983c9171d27c4df41b4b407ec42227845a0
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-28 22:03:57 -0500

    On branch main 	modified:   String_reverse.java 	modified:   sum.java

--- String_reverse.java
+++ String_reverse.java
@@ -1,12 +1,16 @@
+/*
+reversing a input string value
+ */ 
+import java.util.*;
 public class String_reverse {
     public static void main(String[] args){
-
-        String name = "gaurav";
-        
-        for(int i = name.length()-1; i>=0; i--){
-            System.out.print(name.charAt(i));
+        try (Scanner sc = new Scanner(System.in)) {
+            String name = sc.nextLine();
+            
+            for(int i = name.length()-1; i>=0; i--){
+                System.out.print(name.charAt(i));
+            }
         }
-        
     }
     
 }
--- sum.java
+++ sum.java
@@ -1,16 +1,18 @@
+/*
+ * Basic i/o code.
+ */
 import java.util.*;
-
 class sum{
     public static void main(String[] args) {
-        Scanner lelo = new Scanner(System.in);
+        try (Scanner lelo = new Scanner(System.in)) {
+            System.out.println("Enter first number");
+            int num1 = lelo.nextInt();
+            System.out.println("Enter second number");
+            int num2 = lelo.nextInt();
 
-        System.out.println("Enter first number");
-        int num1 = lelo.nextInt();
-        System.out.println("Enter second number");
-        int num2 = lelo.nextInt();
+            int sum = num1 + num2;
 
-        int sum = num1 + num2;
-
-        System.out.println("The sum is : "+sum );
+            System.out.println("The sum is : "+sum );
+        }
     }
 }
