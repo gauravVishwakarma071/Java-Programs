fb8d75685d17d6b5f8cfaf04ec51ba87c1accfbc
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-30 15:23:38 -0500

    On branch main 	modified:   CountingOccurence.java

--- CountingOccurence.java
+++ CountingOccurence.java
@@ -1,11 +1,9 @@
 /*
  * To Count number of occurence of a digit in a number.
  */
-import java.util.*;
 public class CountingOccurence {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-
+       
             int n = 122263;
 
             int count = 0;
@@ -16,5 +14,4 @@ public class CountingOccurence {
             }
             System.out.println(count);
         }
-    }
 }
