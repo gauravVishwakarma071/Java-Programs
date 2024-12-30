d9fd1d3d864bfb654dbbdbc1423714fceddd4ef3
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-30 12:56:01 -0500

    On branch main 	new file:   FibonacciNum.java

---
+++ FibonacciNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FibonacciNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number..?");
+        int n = sc.nextInt();
+        System.out.println("The "+n+"th fibinacci number is : "+fibo(n));
+        
+    }
+        
+    public static int fibo(int n) {
+        if(n==0) return 0;
+		if(n==1||n==2) return 1;
+        return fibo(n-1) + fibo(n-2);  
+    }
+}
