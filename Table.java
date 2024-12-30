3c7ef203d8fcf11136636cb3d4ec978f5eeba4ac
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-29 23:32:44 -0500

    On branch main 	modified:   Table.java

--- Table.java
+++ Table.java
@@ -1,18 +1,19 @@
-//Table of any number.
-
+/*
+ * Table of any number.
+ */
 import java.util.*;
 
 public class Table {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter a number..!");
-        int num = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number..!");
+            int num = sc.nextInt();
 
-        int i=1;
-        while(i<11){
-            System.out.println(num+" * "+i+" = "+i*num);
-            i++;
+            int i=1;
+            while(i<11){
+                System.out.println(num+" * "+i+" = "+i*num);
+                i++;
+            }
         }
     }
 }
