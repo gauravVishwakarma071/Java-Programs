39ccecdfd7dc9571e1782affee5413a9eacec6e5
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-31 15:43:01 -0500

    On branch main 	modified:   Switch.java

--- Switch.java
+++ Switch.java
@@ -4,16 +4,17 @@
 import java.util.*;
 public class Switch {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int btn = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number");
+            int btn = sc.nextInt();
 
-        switch (btn) {
-            case 1 -> System.out.println("Hello");
-            case 2 -> System.out.println("Namaste");
-            case 3 -> System.out.println("bonjur");
-            default -> {
-                System.out.println("Invalid Button");
+            switch (btn) {
+                case 1 -> System.out.println("Hello");
+                case 2 -> System.out.println("Namaste");
+                case 3 -> System.out.println("bonjur");
+                default -> {
+                    System.out.println("Invalid Button");
+                }
             }
         }
 
