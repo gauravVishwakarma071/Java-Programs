e89595906e05f7385504e09312ab72ebf0000829
Gaurav <gauravvishwakarma071@gmail.com>
2024-12-31 15:39:00 -0500

    On branch main 	modified:   reverseNum.java

--- reverseNum.java
+++ reverseNum.java
@@ -1,11 +1,14 @@
+/*
+ * Reversing a given number.
+ */
 import java.util.*;
 public class reverseNum {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a number...?");
-        int n = sc.nextInt();
-        revNum(n);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number...?");
+            int n = sc.nextInt();
+            revNum(n);
+        }
     }
 
     public static void revNum(int n){
