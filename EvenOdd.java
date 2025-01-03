ecd03410cd06713386f0075c6c858e9b70d38565
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-02 22:02:27 -0500

    Even or Odd

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+/*
+ * De fine a method to find out thata givan number is Even or Odd..?
+ */
+import java.util.*;         
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a number..?");
+        int a = in.nextInt();
+        evenOdd(a);
+    }
+    
+    public static void evenOdd(int a){
+        if(a%2==0)System.out.println("EVEN");
+        else  System.out.println("ODD");
+    }
+}
