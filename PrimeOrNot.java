7dce4da6b59f4a4c9c9f7bd5bb625fce311c0dcb
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-04 21:21:56 -0500

    Methods to Write Prime Number Program in Java

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -4,13 +4,13 @@
 import java.util.*;
 public class PrimeOrNot {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a number to check prime or not");
-        int n = sc.nextInt();
-        
-        if(primeCheck(n)==true) System.out.println("PRIME");
-        if(primeCheck(n)==false) System.out.println("NOT PRIME");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number to check prime or not");
+            int n = sc.nextInt();
+            
+            if(primeCheck(n)==true) System.out.println("PRIME");
+            if(primeCheck(n)==false) System.out.println("NOT PRIME");
+        }
 
     }   
 
