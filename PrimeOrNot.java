e8d7cdf54819d275f893117d244f361f64c14ae7
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-06 23:00:49 -0500

    To check if a given triplet is a Pythagorean triplet or not.
    	modified:   PrimeOrNot.java

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -20,7 +20,7 @@ public class PrimeOrNot {
             return false;
         }
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < (n/2)+1 ; i++) {
             if(n%i==0) return false;
         }
         return true;
