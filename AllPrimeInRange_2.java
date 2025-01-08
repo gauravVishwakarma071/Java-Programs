e8ed93a2d6ed842219d0ed146c96fb09b80a2618
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-07 20:05:52 -0500

    Write a function to print all the prime number in between given range.

---
+++ AllPrimeInRange_2.java
@@ -0,0 +1,27 @@
+//Write a function to print all the prime number in between given range.
+import java.util.*;
+public class AllPrimeInRange_2 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter initial range");
+            int a = sc.nextInt();
+            System.out.println("Enter final range");
+            int b = sc.nextInt();
+
+            allPrime(a, b);
+            
+        }       
+    }
+
+    public static void  allPrime(int a, int b){
+        boolean ans = true;
+        for (int i = a; i < b; i++) {
+            for (int j = 2; j < (i/2)+1 ; j++) {
+                if(i%j==0){
+                    ans = false;
+                }
+            }
+            if(ans==true) System.out.print(i+" ");
+        }
+    }
+}
