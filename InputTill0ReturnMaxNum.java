6777d87a9123b192348a009122d534268066dd0d
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-10 23:40:53 -0500

    Take integer inputs till the user enters 0 and print the largest number from all.

--- InputTill0ReturnMaxNum.java
+++ InputTill0ReturnMaxNum.java
@@ -1,22 +1,25 @@
+//Take integer inputs till the user enters 0 and print the largest number from all.
 import java.util.Scanner;
 
 public class InputTill0ReturnMaxNum {
     public static void main(String[] args) {
         solution();
+        
     }
 
     public static int solution(){
         try (Scanner sc = new Scanner(System.in)) {
-            int max = 0;
+            int max=0;
             int n;
 
             while (true) { 
             n = sc.nextInt();
               
             if(max<n){
-                n = max;
+                max = n;
             }
             if(n==0){
+                System.out.println("The largest number among them is : "+max);
                 return max;
             }   
             }
