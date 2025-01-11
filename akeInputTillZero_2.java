fb3b5e93f74a700b331510701089c7c12f3f31a1
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-10 23:40:30 -0500

    Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)

--- akeInputTillZero_2.java
+++ akeInputTillZero_2.java
@@ -1,4 +1,4 @@
-//Input a number and print all the factors of that number (use loops).
+//Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
 import java.util.*;
 public class akeInputTillZero_2{
     public static void main(String[] args) {
@@ -11,11 +11,11 @@ public class akeInputTillZero_2{
         try (Scanner sc = new Scanner(System.in)) {
             int sum = 0;
             int n;
-
             while (true) { 
              n = sc.nextInt();
              sum += n;
               if(n==0){
+                System.out.println("The sum of all numbers are : "+sum);
                 return sum;
               }   
             }
