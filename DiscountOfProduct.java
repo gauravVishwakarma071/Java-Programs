4ce770a4f929ed73885eafe733f11a0305fb64f9
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-11 23:05:07 -0500

    Calculate Discount Of Product.

--- DiscountOfProduct.java
+++ DiscountOfProduct.java
@@ -2,14 +2,14 @@
 import java.util.*;
 public class DiscountOfProduct {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter price");
-    int price = sc.nextInt();
-    System.out.println("Enter discount percentage");
-    int discountP = sc.nextInt();
+    try (Scanner sc = new Scanner(System.in)) {
+        System.out.println("Enter price");
+        int price = sc.nextInt();
+        System.out.println("Enter discount percentage");
+        int discountP = sc.nextInt();
 
-    float discount = (float) price*discountP / 100;
-    System.out.println("The final price : "+ (price-discount));
+        float discount = (float) price*discountP / 100;
+        System.out.println("The final price : "+ (price-discount));
+    }
     }
 }
