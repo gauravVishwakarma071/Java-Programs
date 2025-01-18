87560282f7c9e18987db495252bee94a15a84271
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-17 23:17:29 -0500

    To find a value(x) in an array.

---
+++ FindXInAnArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FindXInAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter : ");
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.print("Enter Your X : ");
+        int x = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==x) System.out.println("At index : " + i);
+        }
+
+    }
+}
