cd4d291ae3b223fcc259e562586d617a5d0a4fdd
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-22 23:06:36 -0500

    Printing n natural number.(File : 1)

---
+++ Z_nNaturalNumber.java
@@ -0,0 +1,15 @@
+//File : 1
+import java.util.*;
+public class Z_nNaturalNumber {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+
+        System.out.print("Enter n :");
+        int n = c.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
