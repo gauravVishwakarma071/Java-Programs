1512a6b3b2bc90b743952047d526f48fd35cb4b4
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-25 22:57:30 -0500

    Shuffle the elements of an array.

--- ShuffleArray.java
+++ ShuffleArray.java
@@ -3,25 +3,28 @@ import java.util.*;
 public class ShuffleArray{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println();
-        int arr[] = new int[4];
+        System.out.print("Enter length of array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
         System.out.println("Give inputs in array");
 
         //Taking inputs in arrays
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print("Enter at "+i+" : ");
             arr[i] = sc.nextInt();
         }
-        int n = (arr.length) / 2;
+        shuffle(arr, n/2);
         sc.close();
     }
-    public static int[] shuffle(int[] arr, int n) {
+    public static void shuffle(int[] arr, int n) {
         //creating an array to store results
+        int index = 0;
         int result[] = new int[arr.length];
         for (int i = 0; i < n; i++) {
-            result[i] = arr[i];
-            result[i*n] = arr[i*n];
+            result[index++] = arr[i];
+            result[index++] = arr[i+n];
         }
-        return arr; 
+        System.out.println(Arrays.toString(result)); 
     }
-}
+}//Input: nums = [2,5,1,3,4,7], n = 3
+//Output: [2,3,5,4,1,7]
