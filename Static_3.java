7905b4da35f9939717c1cc29a490e1bc0481902c
Gaurav <gauravvishwakarma071@gmail.com>
2025-03-07 16:47:29 -0500

    3_Larning Static keyword.

---
+++ Static_3.java
@@ -0,0 +1,8 @@
+public class Static_3 {
+    static{
+        System.out.println("static block executed!");
+    }
+    public static void main(String[] args) {
+        System.out.println("main block executed!");        
+    }
+}
