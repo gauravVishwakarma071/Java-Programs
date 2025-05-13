fdc91ed8f519c32b0f31d6713fbe37187860eb37
Gaurav <gauravvishwakarma071@gmail.com>
2025-05-13 14:44:44 -0400

    Multihreading concept.

---
+++ MyThread02.java
@@ -0,0 +1,45 @@
+public class MyThread02 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        CThread t1 = new CThread(counter);
+        CThread t2 = new CThread(counter);
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+
+        }
+        System.out.println(counter.getCounter());
+    }
+}
+
+class Counter {
+    int count;
+
+    void increment() {
+        count++;
+    }
+
+    int getCounter() {
+        return count;
+    }
+}
+
+class CThread extends Thread {
+    Counter counter;
+
+    CThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
