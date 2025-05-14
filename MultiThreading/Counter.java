1d7f8792c096eff0a05cbebf0dc7d57021514756
Gaurav <gauravvishwakarma071@gmail.com>
2025-05-14 12:18:42 -0400

    Multihreading concept.

---
+++ MultiThreading/Counter.java
@@ -0,0 +1,14 @@
+package MultiThreading;
+
+public class Counter {
+    private int count;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCounter() {
+        return count;
+    }
+
+}
--- MultiThreading/multiTreading02.java
+++
@@ -1,7 +0,0 @@
-package MultiThreading;
-
-public class multiTreading02 {
-    public static void main(String[] args) {
-
-    }
-}
