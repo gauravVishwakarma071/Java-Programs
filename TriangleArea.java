37844e6edbc6aaf4edadb127bd2e65fb6994e3db
Gaurav <gauravvishwakarma071@gmail.com>
2025-06-03 18:12:19 -0400

    //Wap to find the area of a triangle using class and object.

--- TriangleArea.java
+++ TriangleArea.java
@@ -1,26 +1,27 @@
+
 //Wap to find the area of a triangle using class and object.
 import java.util.*;
 
-class Triangle{
-    int b ;
+class Triangle {
+    int b;
     int l;
 
-    void area(int b,int c){
-        float result = (b*c)/2;
-        System.out.print("The Area is : "+result);
+    void area(int b, int c) {
+        float result = (b * c) / 2;
+        System.out.print("The Area is : " + result);
     }
 }
 
-public class TriangleArea{
+public class TriangleArea {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            com.inheritence.Triangle t1 = new com.inheritence.Triangle();
+            Triangle t1 = new Triangle();
 
             System.out.print("Enter breathe : ");
-            t1.b  = sc.nextInt();
+            t1.b = sc.nextInt();
 
             System.out.print("Enter length : ");
-            t1.l  = sc.nextInt();
+            t1.l = sc.nextInt();
 
             t1.area(t1.b, t1.l);
         }
