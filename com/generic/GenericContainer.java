ff4af0ae2a0fcf59eecf6946afe4b5b7362f0412
Gaurav <gauravvishwakarma071@gmail.com>
2025-07-22 14:25:44 +0530

    A generic container interface (Learning code).

--- com/generic/GenericContainer.java
+++ com/generic/GenericContainer.java
@@ -1,13 +1,23 @@
 package com.generic;
 
 public class GenericContainer<T> implements Container<T> {
+
     @Override
     public void add(T item) {
-
     }
 
     @Override
     public T get() {
         return null;
+    }
+
+    public static void main(String[] args) {
+        GenericContainer<String> gC = new GenericContainer<>();
+        gC.add("Gaurav");
+
+
+        String name = (String) gC.get();
+        System.out.println(name);
+        System.out.println(gC.get());
     }
 }
