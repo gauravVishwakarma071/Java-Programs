ff0521aa083c513979ebc1e0d7bc0fe271ef6ce7
Gaurav <gauravvishwakarma071@gmail.com>
2025-07-22 14:16:11 +0530

    A generic container interface (Learning code).

---
+++ com/generic/GenericContainer.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class GenericContainer<T> implements Container<T> {
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public T get() {
+        return null;
+    }
+}
